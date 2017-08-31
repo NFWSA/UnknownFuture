b2f216b24f2ef767a1b4591924fc41a84d2ec0d1
SurgeNight <surgenight@hotmail.com>
2017-08-31 15:39:56 +0800

    fix typo in Test/Makefile and continue to implement make.py.

--- Tools/BuildSystem/SCutefile.mk
+++ Tools/BuildSystem/SCutefile.mk
@@ -1,17 +1,23 @@
 project a b c
 compile cxx g++
-compile cxxflags -std=c++11
+compile cxxflags -std=c++11 -I.. -I../..
 compile c gcc
 #fdf
-c {
-	./../a.c
-	b.c
-	c.cpp
+a.target VariantTest
+b.target ColorTableTest
+c.target LoggerTest
+a {
+	../../Test/VariantTest.cpp
+	../../SNVariant/Variant.cpp
+}
+b{
+	../../Test/ColorTableTest.cpp
+	../../SNLogger/ColorOutput/ColorTable.cpp
 }
 
-c.target d
-c.path ./
-
-b{
-	c.c
+c {
+	../../Test/LoggerTest.cpp
+	../../SNLogger/ColorOutput/ColorTable.cpp
+	../../SNLogger/Logger.cpp
+	../../SNLogger/FormatLogger.cpp
 }
